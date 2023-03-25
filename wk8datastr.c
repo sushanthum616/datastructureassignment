b39b0fc9ab8b890e869fce5e83fcc6a399bae012
sushanthum616 <112780051+sushanthum616@users.noreply.github.com>
2023-03-25 12:10:44 -0700

    Update wk8datastr.c

--- wk8datastr.c
+++ wk8datastr.c
@@ -158,3 +158,139 @@ int main()
 
     return 0;
 }
+// question no 7
+#include <stdio.h>
+#include <string.h>
+
+void concatenate_string(char* s, char* s1, size_t max_len)
+{
+    size_t i, j;
+
+    j = strlen(s);
+    for (i = 0; i < max_len && s1[i] != '\0'; i++) {
+        s[i + j] = s1[i];
+    }
+
+    s[i + j] = '\0';
+
+    return;
+}
+
+int main()
+{
+    char s[5000], s1[5000];
+
+    printf("Enter the first string: ");
+    fgets(s, sizeof(s), stdin);
+    s[strcspn(s, "\n")] = '\0'; // remove newline character
+
+    printf("Enter the second string: ");
+    fgets(s1, sizeof(s1), stdin);
+    s1[strcspn(s1, "\n")] = '\0'; // remove newline character
+
+    concatenate_string(s, s1, sizeof(s) - strlen(s) - 1);
+
+    printf("Concatenated String is: '%s'\n", s);
+
+    return 0;
+}
+
+// question no 8
+#include <stdio.h>
+
+void factorial(int *n) {
+    int i;
+    int fact = 1;
+
+    for (i = 1; i <= *n; i++) {
+        fact *= i;
+    }
+
+    printf(" %d factorial is: %d", *n, fact);
+}
+
+int main() {
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    factorial(&n);
+
+    return 0;
+}
+// question no 9
+#include <stdio.h>
+
+int* getMinMax(int arr[], int n) {
+    static int result[2]; // declare static array to hold min and max values
+    result[0] = arr[0]; // initialize minimum value
+    result[1] = arr[0]; // initialize maximum value
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] < result[0]) {
+            result[0] = arr[i]; // update minimum value
+        }
+        if (arr[i] > result[1]) {
+            result[1] = arr[i]; // update maximum value
+        }
+    }
+
+    return result; // return array of min and max values
+}
+
+int main() {
+    int arr[] = {3, 5, 1, 9, 7};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int* result = getMinMax(arr, n);
+
+    printf("Minimum element is %d\n", result[0]);
+    printf("Maximum element is %d\n", result[1]);
+
+    return 0;
+}
+
+// question no 10
+
+#include <stdio.h>
+#include <string.h>
+
+void selection_sort(char arr[][100], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int min_idx = i;
+        for (int j = i + 1; j < n; j++) {
+            if (strcmp(arr[j], arr[min_idx]) < 0) {
+                min_idx = j;
+            }
+        }
+        if (min_idx != i) {
+            char temp[100];
+            strcpy(temp, arr[i]);
+            strcpy(arr[i], arr[min_idx]);
+            strcpy(arr[min_idx], temp);
+        }
+    }
+}
+
+int main() {
+    char names[50][100];
+
+    printf("Enter names: ");
+    for (int i = 0; i < 50; i++) {
+        if (scanf("%s", names[i]) == 1 && strcmp(names[i], "-1") != 0) {
+            continue;
+        }
+        else {
+            selection_sort(names, i);
+            printf("alphabetical list:\n");
+            for (int j = 0; j < i; j++) {
+                printf("%s\n", names[j]);
+            }
+            break;
+        }
+    }
+
+    return 0;
+}
+
